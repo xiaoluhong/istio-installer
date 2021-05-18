e332407682ca4e1ba2cd73e7bda11d31da6dd0a3
xiaolu hong <xiaoluhong@rancher.com>
2021-05-18 20:02:27 +0800

    update 1.8.6-rancher1-1-cn

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:latest
 
-ENV ISTIO_VERSION 1.8.5
+ENV ISTIO_VERSION 1.8.6
 
 RUN apk update && apk add curl bash coreutils jq nginx ca-certificates && rm -rf /var/cache/apk/*
 
--- scripts/create_istio_system.sh
+++ scripts/create_istio_system.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
-istioctl version -i ${ISTIO_NAMESPACE}
+istioctl version -i $ISTIO_NAMESPACE
 
 ISTIO_FILES=("/app/istio-base.yaml")
 if test -f "/app/overlay-config.yaml"; then
-   echo "creating istio system using overlay"
-   ISTIO_FILES+=("/app/overlay-config.yaml")
+    echo "creating istio system using overlay"
+    ISTIO_FILES+=("/app/overlay-config.yaml")
 fi
 
-if [[ ${FORCE_INSTALL} != true ]]; then
+if [[ $FORCE_INSTALL != true ]]; then
   old_versions=$(kubectl get --ignore-not-found=true deploy istio-pilot -n istio-system -o=jsonpath='{$.spec.template.spec.containers[*].image}')
   # check if older version of istio is still installed in istio-system namespace
-  if [[ ${old_versions} == *"1.3"* || ${old_versions} == *"1.4"* || ${old_versions} == *"1.5"* ]]; then
-      echo "please uninstall current istio version in istio-system namespace before attempting to install ${ISTIO_VERSION}"
+  if [[ $old_versions == *"1.3"* || $old_versions == *"1.4"* || $old_versions == *"1.5"* ]]; then
+      echo "please uninstall current istio version in istio-system namespace before attempting to install $ISTIO_VERSION"
       exit 1
   fi
   # force install will skip file checks. same analyze command is valid for install and upgrade
-  istioctl analyze ${ISTIO_FILES} -i ${ISTIO_NAMESPACE} --failure-threshold Error
-  if [[ $? -ne 0 ]]; then
+if ! istioctl analyze $ISTIO_FILES -i $ISTIO_NAMESPACE --failure-threshold Error; then
     echo "error found during istioctl analyze"
     exit 1
   fi
 fi
 
-versions=$(kubectl get --ignore-not-found=true deploy istiod -n ${ISTIO_NAMESPACE} -o=jsonpath='{$.spec.template.spec.containers[*].image}')
-if [[ ${versions} == "" ]]; then
+versions=$(kubectl get --ignore-not-found=true deploy istiod -n $ISTIO_NAMESPACE -o=jsonpath='{$.spec.template.spec.containers[*].image}')
+if [[ $versions == "" ]]; then
   echo "running istioctl install"
-  istioctl install -i ${ISTIO_NAMESPACE} -y ${ISTIO_FILES[@]/#/-f }
-
+  
+  if ! istioctl install -i $ISTIO_NAMESPACE -y ${ISTIO_FILES[@]/#/-f }; then
+    echo "error found during istioctl install"
+    exit 1
+  fi
 else
   echo "running istioctl upgrade"
-  istioctl upgrade -i ${ISTIO_NAMESPACE} -y ${ISTIO_FILES[@]/#/-f }
+  
+  if ! istioctl upgrade -i $ISTIO_NAMESPACE -y ${ISTIO_FILES[@]/#/-f }; then
+    echo "error found during istioctl upgrade"
+    exit 1
+  fi
 fi
 
 if kubectl get namespace cattle-dashboards; then
